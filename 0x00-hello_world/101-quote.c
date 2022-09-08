1c305fa744869cb5cf03fd7819f3b1a0cfe23509
mawuliro <mawuliro2001@gmail.com>
2022-09-08 15:11:41 -0700

    C programs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main is the entry of the program
+ * this program is gonna print out and that piece of art is useful
+ * Return : 1 on success
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-serve as entry point for the program
+ * Return: by default returns zero if no errors otherwise return no-zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
