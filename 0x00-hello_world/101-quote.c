ebb36e0bfbb78d0038c7e527fae8a2532ca17f10
mawuliro <mawuliro2001@gmail.com>
2022-09-08 17:46:08 -0700

    C programs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <unistd.h>
+#include <stdio.h>
+
 /**
- * main-program entry point
- *
- * Return : 1 on success
+ * main -prints exactly "and that piece of art is useful"
+ * - Dora Korpar , 2015-10-19
+ * followed by a new line, to the standard error
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
