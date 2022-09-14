33e09b34b03b4fd3e15b6d6a63b66bf188a461c1
mawuliro <mawuliro2001@gmail.com>
2022-09-13 19:25:33 -0700

    nested loops & functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - this program prints alphabet in 10 lines
+ *
+ * Return: 0 (Success)
+ */
+
+/*void print_alphabet_x10(void)*/
+void print_alphabet_x10(void)
+{
+	char alfa = 'a';
+	int n = 1;
+
+	while (n <= 10)
+	{
+		while (alfa <= 'z')
+		{
+			write(1, $alfa, 1);
+			alfa++;
+		}
+		write(1, "\n", 1);
+		n++;
+		alfa = 'a';
+	}
+}
