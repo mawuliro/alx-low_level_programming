77c9ac11f64493d5ed5ca704f7df83c1d4deb3e9
mawuliro <mawuliro2001@gmail.com>
2022-09-13 19:52:07 -0700

    loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - this program add to integer
+ *
+ * Return: result
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this program print the last digit of a number
+ *
+ * @num: character value
+ *
+ * Return: num
+ */
+
+void time_table(void)
+{
+	int n = 0;
+	int num;
+
+	while (n <= '9')
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			int x = num * n;
+			return (x);
+			_putchar(',');
+		}
+		n++;
+		num = '0';
+	}
+}
