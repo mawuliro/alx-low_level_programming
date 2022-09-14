a76156b684aa4cca0a22897707da16286910bc37
mawuliro <mawuliro2001@gmail.com>
2022-09-13 17:45:48 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - this program prints alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alfa = 'a';
+
+	while (alfa <= 'z')
+	{
+		write(1, &alfa, 1);
+		alfa++;
+	}
+	write(1, "\n", 1);
+}
