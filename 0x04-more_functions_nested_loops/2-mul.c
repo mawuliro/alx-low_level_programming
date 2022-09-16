00256810b71cf6158fc3776d69d6dd93f79f507f
mawuliro <mawuliro2001@gmail.com>
2022-09-16 16:33:53 -0700

    multiplication of two integer

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,8 @@
 
 /**
  * mul - this function multiplies two integers
- *
+ * @a: character value
+ * @b: character value
  * Return: 0
  */
 
@@ -11,5 +12,5 @@ int mul(int a, int b)
 	int result;
 
 	result = a * b;
-	return(result);
+	return (result);
 }
