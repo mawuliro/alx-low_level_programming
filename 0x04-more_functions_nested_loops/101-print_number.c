815651d7237d8896c0dd253ce51a9a48c7a390d2
mawuliro <mawuliro2001@gmail.com>
2022-09-16 18:50:12 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,21 @@
 #include "main.h"
 
-void print_prev_digits(int n);
-
 /**
- * print_number - prints a number using putchar
+ * print_number - prints an integer
  *
- * Return: void
+ * @n: number
  */
+
 void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
+		n = -n;
 	}
-	if (n > 9 || n < -9)
-		print_prev_digits(n);
-	if (n < 0)
-		_putchar(((n % 10) * -1) + '0');
-	else
-		_putchar((n % 10) + '0');
-}
-
-/**
- * print_prev_digits - recursive function to print all previous digit
- *
- * @n: number to print previous digits of
- * Return: void
- */
-void print_prev_digits(int n)
-{
-	if (((n / 10) < -9) || ((n / 10) > 9))
+	if (n / 10 != 0)
 	{
-		print_prev_digits(n / 10);
-		if (n < 0)
-			_putchar((((n / 10) % 10) * -1) + '0');
-		else
-			_putchar(((n / 10) % 10) + '0');
+		print_number(n / 10);
 	}
-	else
-	{
-		if (n < 0)
-			_putchar(((n / 10) * -1) + '0');
-		else
-			_putchar((n / 10) + '0');
-	}
+	_putchar(n % 10 + '0');
 }
