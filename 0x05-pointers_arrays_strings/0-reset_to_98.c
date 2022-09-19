b2e11f9972635cda2da5ff917c819eefe34115d1
mawuliro <mawuliro2001@gmail.com>
2022-09-19 16:09:39 -0700

    directing a pointer to an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - direct a pointer to an integer and
+ * change the value too 98
+ *
+ * @n: the variable pointing to an integer
+ *
+ * return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
